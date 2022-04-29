e52554663f18a86277ad61cca0029430872cf502
Lalit Sharma <dev.lalit.sh@gmail.com>
2022-04-30 00:23:27 +0530

    Home

--- src/Views/Home/index.js
+++ src/Views/Home/index.js
@@ -1,37 +1,14 @@
-import { connect } from "react-redux";
-import { bindActionCreators } from "@reduxjs/toolkit";
-import { AuthenticateUser, Logout } from "../../Redux/actions/identity.action";
 
-
-const Home = (props) => {
+const Home = () => {
     return (
         <div className="container-fluid">
             <div className="row">
                 <div className="col-sm-12">
-                    this is home
-                        <>
-                            <h1>Logout</h1>
-                            <button 
-                                className="btn btn-warning" 
-                                onClick={() => props.Logout()}
-                            >
-                                Logout
-                            </button>
-                        </>
+                    <h1>this is home</h1>
                 </div>
             </div>
         </div>
     )
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({
-    AuthenticateUser,
-    Logout
-}, dispatch)
-
-const mapStateToProps = ({identity}) => ({
-    identity
-});
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default Home;
